5ff0629b039ff3720748afc66188eb96f912e8d4
Joe Cera <subscribeto@runbox.com>
2019-08-29 17:08:38 -0700

    working on dropdown and bars needs filtering

--- src/App.js
+++ src/App.js
@@ -1,19 +1,83 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+    state = {
+        rates: [
+            {
+                label: "EUR",
+                value: 1,
+                percentage: 1,
+            },
+        ],
+        base: "EUR",
+    };
+
+    componentDidMount = (ev) => {
+        this.onRefresh();
+    }
+
+    createBar = () => {
+
+    }
+
+    onRefresh = () => {
+        let newBase = document.querySelector('#baseCurrency').value
+
+        let url = 'https://api.exchangeratesapi.io/latest?base=' + newBase
+        console.log(url)
+        fetch(url)
+          .then(response => response.json())
+          .then(data => {
+              const newRates = [];
+              let percentage = 1;
+              for (const [key, value] of Object.entries(data.rates)) {
+                  percentage = (1/value) * 100;
+                  newRates.push({
+                      number: value,
+                      label: key,
+                      percentage: percentage,
+                  })
+              }
+
+
+            this.setState({
+                rates: newRates,
+                base: newBase
+            })
+            console.log('this is from onRefresh')
+              console.log(newRates)
+            });
+
+        };
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="MainContent">
+              <div className="MainTitle">
+                  <h1 className="MainTitle--title">The Exchanger - Base: {this.state.base}</h1>
+                  <p className="MainTitle--baseInfo">Select your base currency:</p>
+                  <select className="MainTitle--baseInfo" id="baseCurrency" value={this.state.base} onChange={this.onRefresh}>
+                  {this.state.rates.map(function(item) {
+                      return(
+                      <option value={item.label}>{item.label}</option>
+                  )})}
+                  </select>
+            </div>
+            <div className="GraphArea">
+                    {this.state.rates.map(function(item) {
+                        return(
+                            <div className="GraphArea--barChart" style={{height: item.percentage + "%"}}>{item.label} : {item.number}</div>
+                        )
+                    })}
+
+            </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
+
     );
   }
 }
